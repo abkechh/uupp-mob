b76d55205ef9526218eabbca0ab43d6555348045
abhinavnexvega <abhinav.kumar@nexvega.com>
2025-02-01 00:51:26 +0530

    sc

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -16,7 +16,7 @@ export default function RootLayout() {
   const [loaded] = useFonts({
     SpaceMono: require('../assets/fonts/SpaceMono-Regular.ttf'),
   });
-
+//comment to test commit
   useEffect(() => {
     if (loaded) {
       SplashScreen.hideAsync();
